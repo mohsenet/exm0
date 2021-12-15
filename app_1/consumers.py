98a353c93dae276d35683ba416deeb3e334ec616
mohsen <mohsen@localhost.localdomain>
2021-12-15 15:25:56 +0330

    consumer recive modifyed

--- app_1/consumers.py
+++ app_1/consumers.py
@@ -26,11 +26,32 @@ class EchoInfo(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         print(text_data)
+
+        selected_data = ''
+        index = text_data.find('/')
+        if index != -1:
+            # selected data
+            selected_data = '#' + text_data[index + 1:]
+            # subject
+            text_data = text_data[:text_data.find('/')]
+
         data = get_list('/home/mohsen/Dropbox/_2021/core.txt', ('[' + text_data + ']' + '\n'))
 
-        d = ''
-        for i in data:
-            d += i + '\n'
+        if selected_data != '':
+            selected_d = ''
+            read_status = '1'
+            for i in data:
+                if i == selected_data:
+                    read_status = '0'
+                if read_status == '0':
+                    selected_d += i + '\n'
+                    if i == '#':
+                        break
+            self.send(text_data=selected_d)
+        else:
+            d = ''
+            for i in data:
+                d += i + '\n'
 
-        self.send(text_data=d)
+            self.send(text_data=d)
 
